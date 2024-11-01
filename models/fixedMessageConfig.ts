9bcbfb81f1ffd9d865a02f826e8b310ea48474e7
akki256 <aki256@outlook.com>
2024-11-02 04:44:50 +0900

    update schema

--- models/fixedMessageConfig.ts
+++ models/fixedMessageConfig.ts
@@ -7,11 +7,13 @@ import { createModel, guildId, messageOptionSchema } from './util';
 const zodSchema = BaseConfigSchema.and(FixedMessageConfig);
 
 const fixedMessageSchema = new Schema<z.infer<typeof zodSchema>>({
-  guildId,
+  guildId: { ...guildId, index: true },
   channel: Schema.Types.String,
   enabled: Schema.Types.Boolean,
   message: messageOptionSchema,
   last: Schema.Types.String,
 });
+
+fixedMessageSchema.index({ guildId: 1, channel: 1 }, { unique: true });
 
 export default createModel('fixedMessageConfig', fixedMessageSchema);
